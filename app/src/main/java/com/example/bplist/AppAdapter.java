7699c5c610583410732cebd465cc620e274d8aa1
Tru Nguyen <trunguyen@Trus-MacBook-Pro.local>
2019-02-20 15:31:10 -0600

    pg:34

--- app/src/main/java/com/example/bplist/AppAdapter.java
+++ app/src/main/java/com/example/bplist/AppAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -35,22 +36,31 @@ public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
         return mReadings.size();
     }
 
-    public static class AppViewHolder extends RecyclerView.ViewHolder {
+    public static class AppViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView mReading, mDate;
+        private BloodPressure mBloodPressure;
 
         public AppViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_layout, parent, false));
 
+            itemView.setOnClickListener(this);
             mReading = itemView.findViewById(R.id.text_reading);
             mDate = itemView.findViewById(R.id.text_date);
         }
 
         public void bind(BloodPressure bloodPressure) {
+            mBloodPressure = bloodPressure;
             mReading.setText(bloodPressure.getSystolic() + " / " + bloodPressure.getDiastolic() +
              " - " + bloodPressure.getCategory());
 
             mDate.setText(bloodPressure.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View v)
+        {
+            Toast.makeText(itemView.getContext(), "Clicked" + mBloodPressure.getCategory(), Toast.LENGTH_SHORT).show();
         }
     }
 }
